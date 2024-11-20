7166858b4fc1a926cfd9f6d66b45184b965a74b8
Adam Leigh <adam@fluently.tech>
2024-11-20 15:21:05 +0200

    test: refactored fixture to create an instance of the class with difficulty as a parameter.
    Wrote the tests to ensure that the class rejects input other than the strings "easy", "medium" or "hard". Raises ValueError instead of creating a successful instance of the object.

--- tests/test_number_game.py
+++ tests/test_number_game.py
@@ -1,11 +1,25 @@
 import pytest
 from src.task2.number_game import NumberGuessingGame
+from typing import Any
 
 @pytest.fixture
 def game():
     """Creates an instance of the NumberGuessingGame object for testing."""
-    return NumberGuessingGame()
+    return NumberGuessingGame(difficulty="easy")
 
 def test_init(game):
     """Verified that game is instantiated and not a Nonetype."""
     assert game is not None
+
+def test_instantiation_with_difficulty_parameter():
+    VALID_OPTIONS: list[str] = ["easy", "medium", "hard"]
+
+    for difficulty in VALID_OPTIONS:
+        game = NumberGuessingGame(difficulty=difficulty)
+        assert game.difficulty == difficulty
+
+    INVALID_OPTIONS: list[Any] = ["very easy", 123, None, "", [], {}, True]
+
+    for invalid in INVALID_OPTIONS:
+        with pytest.raises(ValueError):
+            NumberGuessingGame(difficulty=invalid)
