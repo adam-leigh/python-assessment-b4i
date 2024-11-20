0a427b0c341c22b26423acc65de5bb874736d3b1
Adam Leigh <adam@fluently.tech>
2024-11-20 23:11:59 +0200

    feat: add Visualizer class with placeholder method for plotting the grades on a bar chart

---
+++ src/task1/visualizations.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Visualizer:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+
+    def plot_grades(self):
+        """Placeholder method to plot grades. Implementing soon."""
+        pass
