91d0b7edaaca40bbff8d66a8c12ef4c2157b75de
Adam Leigh <adam@fluently.tech>
2024-11-20 12:10:04 +0200

    refactor: convert palindrome_checker function into PalindromeChecker class
    For the moment, I've merely indented the palindrome_checker function
    within the class definition and added self as another parameter.
    
    I'm motivated to refactor the project out of a desire to align my code
    with the single responsibility principle for functions. A lot of the
    SRP functions I can extract from the function as it stands are helper
    functions, and could do with being private methods in a class.
    I'm also convicted it will make testing the code easier in the long run.

--- src/task3/palindrome.py
+++ src/task3/palindrome.py
@@ -1,10 +1,12 @@
+class PalindromeChecker:
+    """Class to check if a given string is a palindrome."""
 
-def palindrome_checker(input_string: str) -> bool:
-    """Expects a string as input, then evaluates whether or not that string is a palindrome."""
-    if not (isinstance(input_string, str) and input_string.isalpha()):
-        raise ValueError("Input must be a string containing only alphabetic characters")
-    uniform_string: str = input_string.lower()
-    return uniform_string == uniform_string[::-1]
+    def palindrome_checker(self, input_string: str) -> bool:
+        """Expects a string as input, then evaluates whether or not that string is a palindrome."""
+        if not (isinstance(input_string, str) and input_string.isalpha()):
+            raise ValueError("Input must be a string containing only alphabetic characters")
+        uniform_string: str = input_string.lower()
+        return uniform_string == uniform_string[::-1]
 
 if __name__ == "__main__":
     pass
