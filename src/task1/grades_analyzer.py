a16a9f6beab84561d7e02cd379ec4eadc88db522
Adam Leigh <adam@fluently.tech>
2024-11-20 21:43:06 +0200

    fix: rewrote json handler to use json module over pandas

--- src/task1/grades_analyzer.py
+++ src/task1/grades_analyzer.py
@@ -1,17 +1,23 @@
 import pandas as pd
 
 class Loader:
-
-    def __init__(self) -> None:
+    def __init__(self):
         self.loaders = {
-                ".csv": pd.read_csv,
-                ".json": pd.read_json,
-                ".xlsx": pd.read_excel,
-                }
+            ".csv": pd.read_csv,
+            ".json": self._load_json,
+            ".xlsx": pd.read_excel,
+        }
 
-    def load_data(self, filepath: str) -> pd.DataFrame:
-        extension = filepath.split('.')[-1].lower() # splits the string into a list, indexing the last item in the list.
+    def load_data(self, file_path: str) -> pd.DataFrame:
+        extension = file_path.split('.')[-1].lower()
         if f".{extension}" in self.loaders:
-            return self.loaders[f".{extension}"](filepath)
+            return self.loaders[f".{extension}"](file_path)
         raise ValueError(f"Unsupported file format: {extension}")
+
+    def _load_json(self, file_path: str) -> pd.DataFrame:
+        import json
+
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            return pd.DataFrame(data["grades"])
 
