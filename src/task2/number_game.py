b29ceac309aacc8c57e37c8381946fdf2590ef7c
Adam Leigh <adam@fluently.tech>
2024-11-20 15:23:12 +0200

    refactor: added difficulty parameter to constructor and implemented basic validation to ensure the only valid arguments are accepted.

--- src/task2/number_game.py
+++ src/task2/number_game.py
@@ -1,5 +1,7 @@
 
 class NumberGuessingGame:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, difficulty: str) -> None:
+        if not isinstance(difficulty, str) or difficulty not in ["easy", "medium", "hard"]:
+            raise ValueError("Difficulty must be 'easy', 'medium' or 'hard'.")
+        self.difficulty = difficulty
