5446840514cf2c16c7cc002393c672bfee8fb21d
Adam Leigh <adam@fluently.tech>
2024-11-20 21:16:40 +0200

    test: develop Loader tests to cover JSON and Excel files

--- tests/test_grades_analyzer.py
+++ tests/test_grades_analyzer.py
@@ -6,8 +6,29 @@ from task1.grades_analyzer import Loader
 def csv_file():
     return "data/grades.csv"
 
+@pytest.fixture
+def json_file():
+    return "data/grades.json"
+
+@pytest.fixture
+def excel_file():
+    return "data/grades.xlsx"
+
 def test_loader_can_load_csv(csv_file):
     loader = Loader()
     data = loader.load_data(csv_file)
     assert isinstance(data, pd.DataFrame)
     assert list(data.columns) == ["student_id", "subject", "grade"]
+
+def test_loader_can_load_json(json_file):
+    loader = Loader()
+    data = loader.load_data(json_file)
+    assert isinstance(data, pd.DataFrame)
+    assert list(data.columns) == ["student_id", "subject", "grade"]
+
+def test_loader_can_load_excel(excel_file):
+    loader = Loader()
+    data = loader.load_data(excel_file)
+    assert isinstance(data, pd.DataFrame)
+    assert list(data.columns) == ["student_id", "subject", "grade"]
+
