9cd4b52cbc9f56355c500ae8e42559f9868e166c
Adam Leigh <adam@fluently.tech>
2024-11-20 11:16:50 +0200

    feat: implement basic logic to pass the initial test
    Added minimal logic to the palindrome_checker function to make the
    test pass.

---
+++ src/task3/palindrome.py
@@ -0,0 +1,6 @@
+def palindrome_checker(string: str) -> bool:
+    """Expects a string as input, then evaluates whether or not that string is a palindrome."""
+    return True
+
+if __name__ == "__main__":
+    pass
--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -1,2 +1,6 @@
 from task3.palindrome import palindrome_checker
 
+def test_palindrome_returns_true_for_radar():
+    """ We're expecting the output of this function to return true if a string such as radar is passed into it. """
+    result: bool = palindrome_checker("radar")
+    assert result == True
