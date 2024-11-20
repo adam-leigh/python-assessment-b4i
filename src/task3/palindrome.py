05a70b32a720ef686b03551e8547f2f077358ce0
Adam Leigh <adam@fluently.tech>
2024-11-20 11:29:08 +0200

    test: add test to verify handling of uppercase palindromes
    returns True for uppercase palindromes. The .lower() method should be
    sufficient for now.

--- src/task3/palindrome.py
+++ src/task3/palindrome.py
@@ -1,6 +1,7 @@
 def palindrome_checker(string: str) -> bool:
     """Expects a string as input, then evaluates whether or not that string is a palindrome."""
-    return True
+    uniform_string: str = string.lower()
+    return uniform_string == uniform_string[::-1]
 
 if __name__ == "__main__":
     pass
--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -1,6 +1,34 @@
 from task3.palindrome import palindrome_checker
 
-def test_palindrome_returns_true_for_radar():
-    """ We're expecting the output of this function to return true if a string such as radar is passed into it. """
+def test_palindrome_checker_returns_true_for_radar():
+    """Test that the function returns True for a palindrome string."""
     result: bool = palindrome_checker("radar")
-    assert result == True
+    assert result
+
+def test_palindrome_checker_returns_true_for_valid_palindromes():
+    """Test that the function returns True for a palindrome string."""
+    result: bool = palindrome_checker("civic")
+    assert result
+
+    result: bool = palindrome_checker("racecar")
+    assert result
+
+    result: bool = palindrome_checker("dad")
+    assert result
+
+    result: bool = palindrome_checker("madam")
+    assert result
+
+    result: bool = palindrome_checker("stats")
+    assert result
+
+def test_palindrome_checker_accounts_for_uppercase_palindromes():
+    """Test that the function returns True for uppercase palindrome strings."""
+    result: bool = palindrome_checker("Civic")
+    assert result
+
+def test_palindrome_checker_raises_exception_if_argument_is_not_string():
+    """Test that an exception is raised if the input is not a string."""
+    pass
+
+
