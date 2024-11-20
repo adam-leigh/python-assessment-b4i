68f68e29525aa92761b4cc8f56926010e7f2ab36
Adam Leigh <adam@fluently.tech>
2024-11-20 21:12:17 +0200

    feat: getting Loader class and load_data method to successfully load CSV files.

--- src/task1/grades_analyzer.py
+++ src/task1/grades_analyzer.py
@@ -1,4 +1,5 @@
+import pandas as pd
 
 class Loader:
-    def load_data(self, filepath: str):
-        pass
+    def load_data(self, filepath: str) -> pd.DataFrame:
+        return pd.read_csv(filepath)
