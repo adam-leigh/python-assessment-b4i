0637d2f302b8711d7270fbef97366a9566e6d454
Adam Leigh <adam@fluently.tech>
2024-11-20 21:31:51 +0200

    feat: add JSON and Excel support to Loader object

--- src/task1/grades_analyzer.py
+++ src/task1/grades_analyzer.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
 class Loader:
+
+    def __init__(self) -> None:
+        self.loaders = {
+                ".csv": pd.read_csv,
+                ".json": pd.read_json,
+                ".xlsx": pd.read_excel,
+                }
+
     def load_data(self, filepath: str) -> pd.DataFrame:
-        return pd.read_csv(filepath)
+        extension = filepath.split('.')[-1].lower() # splits the string into a list, indexing the last item in the list.
+        if f".{extension}" in self.loaders:
+            return self.loaders[f".{extension}"](filepath)
+        raise ValueError(f"Unsupported file format: {extension}")
+
