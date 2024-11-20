4b0fc48174143ef0e20c3323f7096cd58eec7844
Adam Leigh <adam@fluently.tech>
2024-11-20 15:56:52 +0200

    refactor: implement getters and setters for difficulty and user_guess attributes
    Refactored the class to make it more rigorous against malicious actors entering information that will disrupt the integrity of the program.

--- src/task2/number_game.py
+++ src/task2/number_game.py
@@ -2,12 +2,32 @@
 class NumberGuessingGame:
 
     def __init__(self, difficulty: str) -> None:
-        if not isinstance(difficulty, str) or difficulty not in ["easy", "medium", "hard"]:
-            raise ValueError("Difficulty must be 'easy', 'medium' or 'hard'.")
+        self._difficulty = str()
+        self._user_guess = int()
         self.difficulty = difficulty
-        self.user_guess = None
+        
+    @property
+    def difficulty(self) -> str:
+        return self._difficulty
+
+    @difficulty.setter
+    def difficulty(self, value: str):
+        if not isinstance(value, str) or value not in ["easy", "medium", "hard"]:
+            raise ValueError("Difficulty must be 'easy', 'medium' or 'hard'.")
+        self._difficulty = value
     
-    def make_guess(self, number: int) -> None:
-        if not isinstance(number, int):
+    @property
+    def user_guess(self) -> int:
+        return self._user_guess
+
+    @user_guess.setter
+    def user_guess(self, value: int) -> None:
+        if not isinstance(value, int):
             raise ValueError("Guess must be an integer.")
+        self._user_guess = value
+
+    def make_guess(self, number: int):
+        """
+        Receives a number from the user and stores it in the user_guess attribute.
+        """
         self.user_guess = number
